5a8a0e3d8c3b405c2d3d2d8d22cc89889ee612fe
Frederik Tegnander <FRTE@COWI.Com>
2025-01-23 13:42:50 +0100

    Cleaned up code and added analyzer releases documentation

---
+++ DependencyInjection.SourceGenerator.Microsoft/AnalyzerReleases.Shipped.md
@@ -0,0 +1,15 @@
+﻿; Shipped analyzer releases
+; https://github.com/dotnet/roslyn-analyzers/blob/main/src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md
+
+## Release 3.0.0
+
+### New Rules
+
+Rule ID | Category | Severity | Notes
+--------|----------|----------|-------
+DI001 | DependencyInjection | Error | The method '{0}' used for registering services must be static
+DI002 | DependencyInjection | Error | The method '{0}' used for registering services must be public or internal
+DI003 | DependencyInjection | Error | The method '{0}' used for registering services must have exactly one parameter of type System.IServiceProvider
+DI004 | DependencyInjection | Error | The method '{0}' used for registering services cannot return void
+DI005 | DependencyInjection | Error | The method '{0}' used for registering services must have exactly one parameter of type System.IServiceProvider or Microsoft.Extensions.DependencyInjection.IServiceCollection
+
--- DependencyInjection.SourceGenerator.Microsoft/Diagnostics/MethodRegistrationDiagnostics.cs
+++ DependencyInjection.SourceGenerator.Microsoft/Diagnostics/MethodRegistrationDiagnostics.cs
@@ -1,77 +1,76 @@
 using Microsoft.CodeAnalysis;
 
-namespace DependencyInjection.SourceGenerator.Microsoft.Diagnostics
+namespace DependencyInjection.SourceGenerator.Microsoft.Diagnostics;
+
+public static class MethodRegistrationDiagnostics
 {
-    public static class MethodRegistrationDiagnostics
+    private static readonly DiagnosticDescriptor MustBeStatic = new(
+        "DI001",
+        "Method Registration",
+        "Register method '{0}' must be static",
+        "DependencyInjection",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
+    private static readonly DiagnosticDescriptor MustBePublicOrInternal = new(
+        "DI002",
+        "Method Registration",
+        "Register method '{0}' must be public or internal",
+        "DependencyInjection",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
+    private static readonly DiagnosticDescriptor InvalidParameters = new(
+        "DI003",
+        "Method Registration",
+        "Register method '{0}' must have exactly one parameter of type System.IServiceProvider",
+        "DependencyInjection",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
+    private static readonly DiagnosticDescriptor CannotReturnVoid = new(
+        "DI004",
+        "Method Registration",
+        "Register method '{0}' cannot return void",
+        "DependencyInjection",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
+    private static readonly DiagnosticDescriptor MustHaveIServiceCollectionParameter = new(
+        "DI005",
+        "Method Registration",
+        "Register method '{0}' must have exactly one parameter of type System.IServiceProvider or Microsoft.Extensions.DependencyInjection.IServiceCollection",
+        "DependencyInjection",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
+    public static void ReportMustBeStatic(IMethodSymbol methodSymbol, SourceProductionContext context)
     {
-        private static readonly DiagnosticDescriptor MustBeStatic = new DiagnosticDescriptor(
-            "DI001",
-            "Method Registration",
-            "Register method '{0}' must be static",
-            "DependencyInjection",
-            DiagnosticSeverity.Error,
-            isEnabledByDefault: true);
+        var diagnostic = Diagnostic.Create(MustBeStatic, Location.None, methodSymbol.Name);
+        context.ReportDiagnostic(diagnostic);
+    }
 
-        private static readonly DiagnosticDescriptor MustBePublicOrInternal = new DiagnosticDescriptor(
-            "DI002",
-            "Method Registration",
-            "Register method '{0}' must be public or internal",
-            "DependencyInjection",
-            DiagnosticSeverity.Error,
-            isEnabledByDefault: true);
+    public static void ReportMustBePublicOrInternal(IMethodSymbol methodSymbol, SourceProductionContext context)
+    {
+        var diagnostic = Diagnostic.Create(MustBePublicOrInternal, Location.None, methodSymbol.Name);
+        context.ReportDiagnostic(diagnostic);
+    }
 
-        private static readonly DiagnosticDescriptor InvalidParameters = new DiagnosticDescriptor(
-            "DI003",
-            "Method Registration",
-            "Register method '{0}' must have exactly one parameter of type System.IServiceProvider",
-            "DependencyInjection",
-            DiagnosticSeverity.Error,
-            isEnabledByDefault: true);
+    public static void ReportInvalidParameters(IMethodSymbol methodSymbol, SourceProductionContext context)
+    {
+        var diagnostic = Diagnostic.Create(InvalidParameters, Location.None, methodSymbol.Name);
+        context.ReportDiagnostic(diagnostic);
+    }
 
-        private static readonly DiagnosticDescriptor CannotReturnVoid = new DiagnosticDescriptor(
-            "DI004",
-            "Method Registration",
-            "Register method '{0}' cannot return void",
-            "DependencyInjection",
-            DiagnosticSeverity.Error,
-            isEnabledByDefault: true);
+    public static void ReportCannotReturnVoid(IMethodSymbol methodSymbol, SourceProductionContext context)
+    {
+        var diagnostic = Diagnostic.Create(CannotReturnVoid, Location.None, methodSymbol.Name);
+        context.ReportDiagnostic(diagnostic);
+    }
 
-        private static readonly DiagnosticDescriptor MustHaveIServiceCollectionParameter = new DiagnosticDescriptor(
-            "DI005",
-            "Method Registration",
-            "Register method '{0}' must have exactly one parameter of type System.IServiceProvider or Microsoft.Extensions.DependencyInjection.IServiceCollection",
-            "DependencyInjection",
-            DiagnosticSeverity.Error,
-            isEnabledByDefault: true);
-
-        public static void ReportMustBeStatic(IMethodSymbol methodSymbol, SourceProductionContext context)
-        {
-            var diagnostic = Diagnostic.Create(MustBeStatic, Location.None, methodSymbol.Name);
-            context.ReportDiagnostic(diagnostic);
-        }
-
-        public static void ReportMustBePublicOrInternal(IMethodSymbol methodSymbol, SourceProductionContext context)
-        {
-            var diagnostic = Diagnostic.Create(MustBePublicOrInternal, Location.None, methodSymbol.Name);
-            context.ReportDiagnostic(diagnostic);
-        }
-
-        public static void ReportInvalidParameters(IMethodSymbol methodSymbol, SourceProductionContext context)
-        {
-            var diagnostic = Diagnostic.Create(InvalidParameters, Location.None, methodSymbol.Name);
-            context.ReportDiagnostic(diagnostic);
-        }
-
-        public static void ReportCannotReturnVoid(IMethodSymbol methodSymbol, SourceProductionContext context)
-        {
-            var diagnostic = Diagnostic.Create(CannotReturnVoid, Location.None, methodSymbol.Name);
-            context.ReportDiagnostic(diagnostic);
-        }
-
-        public static void ReportInvalidMethodParameter(IMethodSymbol methodSymbol, SourceProductionContext context)
-        {
-            var diagnostic = Diagnostic.Create(MustHaveIServiceCollectionParameter, Location.None, methodSymbol.Name);
-            context.ReportDiagnostic(diagnostic);
-        }
+    public static void ReportInvalidMethodParameter(IMethodSymbol methodSymbol, SourceProductionContext context)
+    {
+        var diagnostic = Diagnostic.Create(MustHaveIServiceCollectionParameter, Location.None, methodSymbol.Name);
+        context.ReportDiagnostic(diagnostic);
     }
 }
